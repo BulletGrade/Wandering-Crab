9fc0c84418da8a5719774d744f8cb9bdb4d53443
BulletGrade <you@example.com>
2020-07-11 19:44:01 -0300

    Organization and bullets

---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bullet;
+    public float[] Direction = {-10, 10};
+    float lastShot;
+
+    void Update()
+    {
+        if (Time.time > lastShot + 10)
+        {
+            Spawner();
+            lastShot = Time.time;
+        }
+    }
+
+    void Spawner()
+    {
+        float randomDirection = Direction[Random.Range(0,1)];
+        Instantiate(bullet, transform.parent.position + new Vector3(randomDirection, 0, 0), Quaternion.identity);
+    }
+}
